f8ce3978331a7dd7e54ef720c501178f00bd6c35
Morten Høfft <mhoefft@gbif.org>
2021-05-20 14:29:49 +0200

    Update filter name to align with API v1

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -9,7 +9,7 @@ var siteConfig = {
        { type: 'equals', key: 'taxonKey', value: 6 },
    		{
         "type":"in",
-        "key": "countryCode",
+        "key": "country",
         "values": ["AE", "AF", "AM", "AZ", "BD", "BH", "BN", "BT", "CN", "CX", "CY", "GE", "HK", "ID", "IL", "IN", "IO", "IQ", "IR", "JO", "JP", "KG", "KH", "KP", "KR", "KW", "KZ", "LA", "LB", "LK", "MM", "MN", "MO", "MV", "MY", "NP", "OM", "PH", "PK", "PS", "QA", "RU", "SA", "SG", "SY", "TH", "TJ", "TL", "TM", "TR", "TW", "UZ", "VN", "YE"]
       },
       {
