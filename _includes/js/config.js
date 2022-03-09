5f63da693734b99f4dcac1679b89828c4f6a6a84
Morten Høfft <mhoefft@gbif.org>
2022-03-09 14:22:55 +0100

    feat: update configuration schema. See https://github.com/gbif/hosted-portals/issues/221

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -1,22 +1,33 @@
-var siteTheme = gbifReactComponents.themeBuilder.extend({baseTheme: 'light', extendWith: {
-  primary: themeStyle.colors.primary
-}});
+var siteTheme = gbifReactComponents.themeBuilder.extend({
+  baseTheme: 'light', extendWith: {
+    primary: themeStyle.colors.primary
+  }
+});
 
 var siteConfig = {
-  rootPredicate: {
-    "type": "and",
-    "predicates": [
-       { type: 'equals', key: 'taxonKey', value: 6 },
-   		{
-        "type":"in",
-        "key": "country",
-        "values": ["AE", "AF", "AM", "AZ", "BD", "BH", "BN", "BT", "CN", "CX", "CY", "GE", "HK", "ID", "IL", "IN", "IO", "IQ", "IR", "JO", "JP", "KG", "KH", "KP", "KR", "KW", "KZ", "LA", "LB", "LK", "MM", "MN", "MO", "MV", "MY", "NP", "OM", "PH", "PK", "PS", "QA", "RU", "SA", "SG", "SY", "TH", "TJ", "TL", "TM", "TR", "TW", "UZ", "VN", "YE"]
-      },
-      {
-        "type": "equals",
-        "key": "notIssues",
-        "value": "COUNTRY_COORDINATE_MISMATCH"
-      }
-    ]
+  routes: {
+    occurrenceSearch: {
+      // The route you are currently using for occurrence search. The language prefix will be added automatically
+      // If you need special routes per language, then you have to add locale specific overwrites. The page language is available as a global variable called `pageLang`
+      route: '/data'
+    }
+  },
+  occurrence: {
+    rootPredicate: {
+      "type": "and",
+      "predicates": [
+        { type: 'equals', key: 'taxonKey', value: 6 },
+        {
+          "type": "in",
+          "key": "country",
+          "values": ["AE", "AF", "AM", "AZ", "BD", "BH", "BN", "BT", "CN", "CX", "CY", "GE", "HK", "ID", "IL", "IN", "IO", "IQ", "IR", "JO", "JP", "KG", "KH", "KP", "KR", "KW", "KZ", "LA", "LB", "LK", "MM", "MN", "MO", "MV", "MY", "NP", "OM", "PH", "PK", "PS", "QA", "RU", "SA", "SG", "SY", "TH", "TJ", "TL", "TM", "TR", "TW", "UZ", "VN", "YE"]
+        },
+        {
+          "type": "equals",
+          "key": "notIssues",
+          "value": "COUNTRY_COORDINATE_MISMATCH"
+        }
+      ]
+    }
   }
 };
